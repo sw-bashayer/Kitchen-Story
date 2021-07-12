b60d0e26c40e53f121f65c8f172f68ac5607b734
Bashayer <twj.bashayer@gmail.com>
2021-07-13 01:29:14 +0300

    Display the food list in the home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,6 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  food_list = [
+    [{ "id": 1, "name": "Potato", image: "1.jpg" }, { "id": 2, "name": "Pasta", image: "2.jpg" }, { "id": 3, "name": "Rice", image: "3.jpg" }],
+    [{ "id": 4, "name": "c", image: "4.jpg" }, { "id": 5, "name": "d", image: "5.jpg" }, { "id": 6, "name": "e", image: "6.jpg" }],
+    [{ "id": 7, "name": "f", image: "7.jpg" }, { "id": 8, "name": "g", image: "8.jpg" }, { "id": 9, "name": "m", image: "9.jpg" }]
+  ]
+  foods_list = [
+    {
+      id: 1,
+      name: "Potato",
+      desc: "ppppp",
+      image: "1.jpg",
+    }
+    ,
+    {
+      id: 2,
+      name: "Pasta",
+      desc: "ppppp",
+      image: "2.jpg",
+    },
+    {
+      id: 3,
+      name: "Rice",
+      desc: "ppppp",
+      image: "3.jpg",
+    },
+    {
+      id: 3,
+      name: "Rice",
+      desc: "ppppp",
+      image: "3.jpg",
+    },
+
+  ]
 
   constructor() { }
 
