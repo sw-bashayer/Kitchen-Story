e27a8fe0ffde25157c0ae3484caef6264fc8d346
Bashayer <twj.bashayer@gmail.com>
2021-07-13 01:48:26 +0300

    add admin sign in page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
+import { AdminComponent } from './components/admin/admin.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'admin', component: AdminComponent },
+
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
